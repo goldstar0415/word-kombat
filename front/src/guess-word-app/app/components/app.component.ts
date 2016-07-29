83ee7f160039399c80bc46b9b50246dae5a2e4d0
goldstar0415 <topdeveloper1212@gmail.com>
2016-07-29 19:47:05 +0300

    Moving navigation to separate component

--- front/src/guess-word-app/app/components/app.component.ts
+++ front/src/guess-word-app/app/components/app.component.ts
@@ -5,6 +5,7 @@ import { ChatComponent } from './chat/chat.component';
 import { LeaderboardsComponent } from './leaderboards/leaderboards.component';
 import { AccountComponent } from './account/account.component';
 import { PageFooterComponent } from './page-footer/page-footer.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 declare let $:any;
 
@@ -14,12 +15,12 @@ const basePath = 'guess-word-app/app/components/';
   selector: 'app',
   templateUrl: basePath + 'app.html',
   styleUrls: [basePath + 'app.css'],
-  directives: [ROUTER_DIRECTIVES, PageFooterComponent]
+  directives: [ROUTER_DIRECTIVES, PageFooterComponent, NavigationComponent]
 })
 @RouteConfig([
   { path: '', name: 'Chat', component: ChatComponent },
   { path: '/leaderboards', name: 'Leaderboards', component: LeaderboardsComponent },
-  { path: '/accout', name: 'Account', component: AccountComponent },
+  { path: '/account', name: 'Account', component: AccountComponent },
 ])
 export class AppComponent implements OnInit {
 
--- front/src/guess-word-app/app/components/navigation/navigation.component.ts
+++ front/src/guess-word-app/app/components/navigation/navigation.component.ts
@@ -1,11 +1,13 @@
 import { Component } from 'angular2/core';
+import { ROUTER_DIRECTIVES} from 'angular2/router';
 
 const basePath = 'guess-word-app/app/components/navigation/';
 
 @Component({
   selector: 'navigation',
   templateUrl: basePath + 'navigation.html',
-  styleUrls: [basePath + 'navigation.css']
+  styleUrls: [basePath + 'navigation.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class NavigationComponent {
 
