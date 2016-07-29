78364699744f7f6141dbc472634c59ee503a831a
goldstar0415 <topdeveloper1212@gmail.com>
2016-07-29 19:27:00 +0300

    Moving footer to separate component

--- front/src/guess-word-app/app/components/app.component.ts
+++ front/src/guess-word-app/app/components/app.component.ts
@@ -4,6 +4,7 @@ import { ROUTER_DIRECTIVES, RouteConfig } from 'angular2/router';
 import { ChatComponent } from './chat/chat.component';
 import { LeaderboardsComponent } from './leaderboards/leaderboards.component';
 import { AccountComponent } from './account/account.component';
+import { PageFooterComponent } from './page-footer/page-footer.component';
 
 declare let $:any;
 
@@ -13,7 +14,7 @@ const basePath = 'guess-word-app/app/components/';
   selector: 'app',
   templateUrl: basePath + 'app.html',
   styleUrls: [basePath + 'app.css'],
-  directives: [ROUTER_DIRECTIVES]
+  directives: [ROUTER_DIRECTIVES, PageFooterComponent]
 })
 @RouteConfig([
   { path: '', name: 'Chat', component: ChatComponent },
---
+++ front/src/guess-word-app/app/components/page-footer/page-footer.component.ts
@@ -0,0 +1,12 @@
+import { Component } from 'angular2/core';
+
+const basePath = 'guess-word-app/app/components/page-footer/';
+
+@Component({
+  selector: 'page-footer',
+  templateUrl: basePath + 'page-footer.html',
+  styleUrls: [basePath + 'page-footer.css']
+})
+export class PageFooterComponent {
+
+}
