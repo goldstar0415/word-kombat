9fb7b6ae4b9a04a6299ec3490ec03d6f76bb7c59
goldstar0415 <topdeveloper1212@gmail.com>
2016-08-25 23:18:43 +0300

    Adding user-details component with basic styles

--- front/src/guess-word-app/app/components/account/account.component.ts
+++ front/src/guess-word-app/app/components/account/account.component.ts
@@ -1,9 +1,23 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
+
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { User } from '../../models/user.model';
+
+const basePath = 'guess-word-app/app/components/account/';
 
 @Component({
   selector: 'account',
-  template: `<p> Account </p>`
+  templateUrl: basePath + 'account.html',
+  styleUrls: [basePath + 'account.css'],
+  directives: [UserDetailsComponent]
 })
-export class AccountComponent {
+export class AccountComponent implements OnInit {
+
+  private currentUser: User;
+
+  ngOnInit() {
+    this.currentUser = new User(1, "random@email.com", "random-name", 200, "images/users/noIco.png", 1);
+    console.log(this.currentUser);
+  }
 
 }
---
+++ front/src/guess-word-app/app/components/account/user-details/user-details.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from 'angular2/core';
+
+import { User } from '../../../models/user.model';
+
+const basePath = 'guess-word-app/app/components/account/user-details/';
+
+@Component({
+  selector: 'user-details',
+  templateUrl: basePath + 'user-details.html',
+  styleUrls: [basePath+ 'user-details.css']
+})
+export class UserDetailsComponent {
+  @Input() private user: User;
+}
