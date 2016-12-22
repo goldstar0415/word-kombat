36bc4ed848627380b32f0dcd6268c05f6297a4ca
goldstar0415 <topdeveloper1212@gmail.com>
2016-12-22 17:31:27 +0200

    Modify user details

--- front/src/guess-word-app/app/boot.ts
+++ front/src/guess-word-app/app/boot.ts
@@ -1,9 +1,14 @@
 /// <reference path="../../../../node_modules/angular2/typings/browser.d.ts" />
 import { bootstrap } from 'angular2/platform/browser';
-import { ROUTER_PROVIDERS } from 'angular2/router';
 import { HTTP_PROVIDERS } from 'angular2/http';
+
+import {
+  ROUTER_PROVIDERS,
+} from 'angular2/router';
  
 import { AppComponent } from './components/app.component';
 
-bootstrap(AppComponent, [ROUTER_PROVIDERS, HTTP_PROVIDERS])
-     .catch(error => console.log(error));
+bootstrap(AppComponent, [
+  ROUTER_PROVIDERS,
+  HTTP_PROVIDERS])
+.catch(error => console.log(error));
