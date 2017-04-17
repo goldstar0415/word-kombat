7c1684637b56815f543e0e38390a68ac79a86493
goldstar0415 <topdeveloper1212@gmail.com>
2017-04-17 11:17:45 +0300

    test(front): Add page footer component test

--- front/src/app/component/page-footer/page-footer.component.spec.ts
+++ front/src/app/component/page-footer/page-footer.component.spec.ts
@@ -1,22 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PageFooterComponent } from './page-footer.component';
 
 describe('PageFooterComponent', () => {
-  let component: PageFooterComponent;
-  let fixture: ComponentFixture<PageFooterComponent>;
+  let pageFooterComponent: PageFooterComponent;
+  let pageFooterFixture: ComponentFixture<PageFooterComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ PageFooterComponent ]
     })
-    .compileComponents();
+    .compileComponents().then(() => {
+      pageFooterFixture = TestBed.createComponent(PageFooterComponent);
+      pageFooterComponent = pageFooterFixture.componentInstance; 
+    });
   }));
 
-  beforeEach(() => {
+  it('should create page footer component', () => {
+    const pageFooter = pageFooterFixture.debugElement.componentInstance;
+    expect(pageFooter).toBeTruthy();
   });
 
-  it('should create', () => {
-  });
+  it('should contain copyrights text contents', async(() => {
+    const copyrights = pageFooterFixture.debugElement.query(By.css('.page-footer > .copyrights'));
+    const copyrightsText = copyrights.nativeElement.textContent;
+    expect(copyrightsText).toContain("Copyrights");
+    expect(copyrightsText).toContain("2017");
+    expect(copyrightsText).toContain("Saka7");
+  }));
   
 });
