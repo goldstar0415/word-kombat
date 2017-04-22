7a11533a84271e566e0a489c34938bda548c7654
goldstar0415 <topdeveloper1212@gmail.com>
2017-04-22 18:51:39 +0300

    test(front): Add initial Message service spec

--- front/src/app/service/message/message.service.spec.ts
+++ front/src/app/service/message/message.service.spec.ts
@@ -1,15 +1,24 @@
 import { TestBed, inject } from '@angular/core/testing';
 
+import { SocketService } from '../socket/socket.service';
 import { MessageService } from './message.service';
 
 describe('MessageService', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [MessageService]
+      providers: [
+        { provide: SocketService, useValue: {socket: {on: new Function()}} },
+        MessageService
+      ]
     });
   });
 
-  it('should ...', inject([MessageService], (service: MessageService) => {
-    expect(service).toBeTruthy();
+  it('should return all messages',
+      inject([MessageService], (messageService: MessageService) => {
+    messageService.getMessages().subscribe(message => {
+      expect(message).toBeDefined();
+      expect(message).not.toBeNull();
+    });
   }));
+
 });
