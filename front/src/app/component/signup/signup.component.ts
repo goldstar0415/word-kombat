d9aed68b4fc32db1f3503b5fac3476e0329c7878
goldstar0415 <topdeveloper1212@gmail.com>
2017-04-29 19:16:25 +0300

    feat(front): Add signup component logic and fields validation

--- front/src/app/component/signup/signup.component.ts
+++ front/src/app/component/signup/signup.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { SignUpRequest } from '../../model/signup-request.model';
+import { AuthService } from '../../service/auth/auth.service';
 
 @Component({
   selector: 'wk-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent {
 
-  constructor() { }
+  usernameErrorMessage: string;
+  emailErrorMessage: string;
+  passwordErrorMessage: string;
+  signUpRequest: SignUpRequest;
+  usernameValidity: string;
+  emailValidity: string;
+  passwordValidity: string;
 
-  ngOnInit() {
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.signUpRequest = new SignUpRequest();
+    this.usernameErrorMessage = "Username format is invalid";
+    this.emailErrorMessage = "Email format is invalid";
+    this.passwordErrorMessage = "Password format is invalid";
+  }
+
+  onSubmit() {
+    this.authService.signUp(this.signUpRequest)
+      .subscribe(res => {
+        this.router.navigate(['/account']);
+      }, error => {
+        this.setValidationError(error);
+      });
+  }
+
+  private setValidationError(error) {
+    if(error.target === 'username') {
+      this.usernameErrorMessage = error.message;
+      this.usernameValidity = "invalid";
+    } else if(error.target === 'email') {
+      this.emailErrorMessage = error.message;
+      this.emailValidity = "invalid";
+    } else if(error.target === 'password') {
+      this.passwordErrorMessage = error.message;
+      this.passwordValidity = "invalid";
+    }
   }
 
 }
