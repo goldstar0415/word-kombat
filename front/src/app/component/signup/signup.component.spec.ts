bba980d87401cbbdff58af41f033546099f15e6f
goldstar0415 <topdeveloper1212@gmail.com>
2017-06-10 16:10:36 +0300

    test(front): Improve signup test coverage

--- front/src/app/component/signup/signup.component.spec.ts
+++ front/src/app/component/signup/signup.component.spec.ts
@@ -4,13 +4,13 @@ import {
   ComponentFixtureAutoDetect,
   TestBed
 } from '@angular/core/testing';
-
 import { MockBackend } from '@angular/http/testing';
 import { Http, BaseRequestOptions } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { FormsModule } from '@angular/forms';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
 
 import { SignupComponent } from './signup.component';
 import { SocketService } from '../../service/socket/socket.service';
@@ -22,6 +22,8 @@ import { WordService } from '../../service/word/word.service';
 describe('SignupComponent', () => {
   let signUpComponent: SignupComponent;
   let signUpFixture: ComponentFixture<SignupComponent>;
+  let authService: AuthService;
+  let authServiceSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -57,10 +59,32 @@ describe('SignupComponent', () => {
   beforeEach(() => {
     signUpFixture = TestBed.createComponent(SignupComponent);
     signUpComponent = signUpFixture.componentInstance;
+    authService = signUpFixture.debugElement.injector.get(AuthService);
+    authServiceSpy = spyOn(authService, 'signUp')
+      .and.returnValue(Observable.from([]));
     signUpFixture.detectChanges();
   });
 
   it('should create component', () => {
     expect(signUpComponent).toBeTruthy();
   });
+  
+  it('should create component', () => {
+    expect(signUpComponent).toBeTruthy();
+  });
+
+  it('should call AuthService.signUp method', () => {
+    signUpComponent.onSubmit();
+    signUpFixture.whenStable().then(() => {
+      expect(authServiceSpy.signUp).toHaveBeenCalled();
+    });
+  });
+
+  it('should call AuthService.signUp method only once', () => {
+    signUpComponent.onSubmit();
+    signUpFixture.whenStable().then(() => {
+      expect(authServiceSpy.signUp.callsCount).toEqual(1);
+    });
+  });
+
 });
