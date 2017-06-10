f426c67910a67a13534eb28f065ff729dda876a9
goldstar0415 <topdeveloper1212@gmail.com>
2017-06-10 13:51:35 +0300

    test(front): Improve user-info test coverage

--- front/src/app/component/account/user-info/user-info.component.spec.ts
+++ front/src/app/component/account/user-info/user-info.component.spec.ts
@@ -1,22 +1,47 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MockBackend } from '@angular/http/testing';
 
+import { Observable } from 'rxjs/Observable';
+
+import { User } from '../../../model/user.model';
 import { UserInfoComponent } from './user-info.component';
 
 describe('UserInfoComponent', () => {
-  let component: UserInfoComponent;
-  let fixture: ComponentFixture<UserInfoComponent>;
+  let userInfoComponent: UserInfoComponent;
+  let userInfoFixture: ComponentFixture<UserInfoComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserInfoComponent ]
+      declarations: [ UserInfoComponent ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
+    userInfoFixture = TestBed.createComponent(UserInfoComponent);
+    userInfoComponent = userInfoFixture.componentInstance;
   });
 
-  it('should create', () => {
+  it('should create component', async(() => {
+    const component = userInfoFixture.debugElement.componentInstance;
+    expect(component).toBeDefined();
+  }));
+
+  it('should create empty User object', () => {
+    userInfoComponent.ngOnInit();
+    expect(userInfoComponent.user).toBeDefined();
   });
   
+  it('should create empty Rank object', () => {
+    userInfoComponent.ngOnInit();
+    expect(userInfoComponent.rank).toBeDefined();
+  });
+
+  it('should initialize chartData with a range', () => {
+    userInfoComponent.ngOnInit();
+    expect(userInfoComponent.chartData).toEqual([0, 100]);
+  });
+
 });
