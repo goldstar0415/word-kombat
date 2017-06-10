b9eef2cd8a9d4d2ecc7ec4d29ed39939e52931d3
goldstar0415 <topdeveloper1212@gmail.com>
2017-06-10 16:10:27 +0300

    test(front): Improve signin test coverage

--- front/src/app/component/signin/signin.component.spec.ts
+++ front/src/app/component/signin/signin.component.spec.ts
@@ -4,13 +4,13 @@ import {
   ComponentFixtureAutoDetect,
   TestBed
 } from '@angular/core/testing';
-
 import { MockBackend } from '@angular/http/testing';
 import { Http, BaseRequestOptions } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
-
 import { FormsModule } from '@angular/forms';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
 
 import { SigninComponent } from './signin.component';
 import { SocketService } from '../../service/socket/socket.service';
@@ -22,6 +22,8 @@ import { WordService } from '../../service/word/word.service';
 describe('SigninComponent', () => {
   let signInComponent: SigninComponent;
   let signInFixture: ComponentFixture<SigninComponent>;
+  let authService: AuthService;
+  let authServiceSpy;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -57,10 +59,28 @@ describe('SigninComponent', () => {
   beforeEach(() => {
     signInFixture = TestBed.createComponent(SigninComponent);
     signInComponent = signInFixture.componentInstance;
+    authService = signInFixture.debugElement.injector.get(AuthService);
+    authServiceSpy = spyOn(authService, 'signIn')
+      .and.returnValue(Observable.from([]));
     signInFixture.detectChanges();
   });
 
   it('should create component', () => {
     expect(signInComponent).toBeTruthy();
   });
+
+  it('should call AuthService.signIn method', () => {
+    signInComponent.onSubmit();
+    signInFixture.whenStable().then(() => {
+      expect(authServiceSpy.signIn).toHaveBeenCalled();
+    });
+  });
+
+  it('should call AuthService.signIn method only once', () => {
+    signInComponent.onSubmit();
+    signInFixture.whenStable().then(() => {
+      expect(authServiceSpy.signIn.callsCount).toEqual(1);
+    });
+  });
+
 });
